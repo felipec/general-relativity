9051f53df64262cb9650f6b1985012c659228449
Felipe Contreras <felipe.contreras@gmail.com>
2022-02-01 04:57:41 -0600

    matrix: swap colums to rows
    Makes more sense computationally.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- matrix.js
+++ matrix.js
@@ -2,13 +2,13 @@ Array.prototype.sumf = function(f) {
   return this.reduce((m, e, i) => m + f(e, i), 0);
 }
 
-// c_ij = a_ik * b_kj
+// c_ij = b_ik * a_kj
 function mul(a, b) {
-  if (a[0].length != b.length) throw 'Bad size';
-  const c = a.map(_ => new Array());
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < b[0].length; j++) {
-      c[i][j] = b.sumf((bk, k) => a[i][k] * bk[j]);
+  if (b[0].length != a.length) throw 'Bad size';
+  const c = b.map(_ => new Array());
+  for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < a[0].length; j++) {
+      c[i][j] = a.sumf((ak, k) => b[i][k] * ak[j]);
     }
   }
   return c;
